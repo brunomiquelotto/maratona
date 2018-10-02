cb0e660c79d1acb7a9f3d2bf69115585be8b54d0
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-10-02 20:30:32 -0300

    CRUD Maratonas

--- database/maratonas.js
+++ database/maratonas.js
@@ -4,4 +4,35 @@ var list = function() {
     return context.select('*').from('TB_COMPETITIONS');
 };
 
-module.exports = { list: list };
+var remove = function(id) {
+    return context.from('TB_COMPETITIONS').where({ CompetitionId: id }).del()
+};
+
+var create = function(payload) {
+    return context.table('TB_COMPETITIONS')
+    .insert({
+        Name: payload.name,
+        DtStart: payload.dtStart,
+        WrongTimePenalty: payload.wrongTimePenalty,
+        FreezeTime: payload.freezeTime,
+        CompetitionTime: payload.competitionTime,
+    });
+};
+
+var get = function(id) {
+    return context.first('*').from('TB_COMPETITIONS').where({CompetitionId: id});
+};
+
+var update = function(id, payload) {
+    return context.from('TB_COMPETITIONS')
+    .where({CompetitionId: id})
+    .update({
+        Name: payload.name,
+        DtStart: payload.dtStart,
+        WrongTimePenalty: payload.wrongTimePenalty,
+        FreezeTime: payload.freezeTime,
+        CompetitionTime: payload.competitionTime,
+    });
+};
+
+module.exports = { list: list, remove: remove, create: create, get: get, update: update };
--- routes/maratonas.js
+++ routes/maratonas.js
@@ -1,4 +1,4 @@
-const { list } = require('../database/maratonas.js');
+const { list, remove, create, get, update } = require('../database/maratonas.js');
 
 module.exports = function(app) {
     app.get('/admin/maratonas', (req, res) => {
@@ -7,11 +7,31 @@ module.exports = function(app) {
         });
     });
     
-    app.get('/admin/nova-maratona', (req, res) => {
-        res.render('/admin/maratonas/nova.ejs');
+    app.post('/admin/maratonas/nova', (req, res) => {
+        create(req.body).then(() => {
+            res.redirect('/admin/maratonas');
+        });
     });
 
-    app.post('/admin/nova-maratona', (req, res) => {
-        
+    app.get('/admin/maratonas/nova', (req, res) => {
+        res.render('admin/maratonas/nova.ejs');
+    });
+
+    app.get('/admin/maratonas/:id/remove', (req, res) => {
+        remove(req.params.id).then(() => {
+            res.redirect('/admin/maratonas');
+        })
+    });
+
+    app.get('/admin/maratonas/:id', (req, res) => {
+        get(req.params.id).then((result) => {
+            res.render('admin/maratonas/edit.ejs', { maratona: result });
+        });
+    });
+
+    app.post('/admin/maratonas/:id', (req, res) => {
+        update(req.params.id, req.body).then(() => {
+            res.redirect('/admin/maratonas');
+        });
     });
 };
