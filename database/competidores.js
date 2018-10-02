cf8b7c85ec5f66f5b600429ad8b837f13a39ba84
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-10-02 19:56:00 -0300

    CRUD times nas maratonas

--- database/competidores.js
+++ database/competidores.js
@@ -4,4 +4,21 @@ var list = function(id) {
     return context.select('*').where({ CompetitionId: id }).from('TB_TEAM');
 };
 
-module.exports = { list: list };
+var create = function (payload) {
+    return context.table('TB_TEAM').insert({ Name: payload.Name, CompetitionId: payload.Id });
+};
+
+var remove = function(competitionId, teamId) {
+    return context.from('TB_TEAM').where({ CompetitionId: competitionId, TeamId: teamId }).del();
+};
+
+var get = function(competitionId, teamId) {
+    return context.first('*').from('TB_TEAM').where({ TeamId: teamId, CompetitionId: competitionId });
+};
+
+var update = function (competitionId, teamId, payload) {
+    return context.from('TB_TEAM').where({ CompetitionId: competitionId, TeamId: teamId })
+        .update({ Name: payload.Name });
+};
+
+module.exports = { list: list, create: create, remove: remove, get: get, update: update };
--- routes/competidores.js
+++ routes/competidores.js
@@ -1,9 +1,43 @@
-const { list } = require('../database/competidores.js');
+const { list, create, remove, get, update } = require('../database/competidores.js');
 
 module.exports = function(app) {
     app.get('/admin/maratonas/:id/competidores', (req, res) => {
         list(req.params.id).then(function(result) {
-            res.render('admin/competidores/index.ejs', { times: result });
+            res.render('admin/competidores/index.ejs', { times: result, id: req.params.id });
+        });
+    });
+
+    app.get('/admin/maratonas/:id/competidores/novo', (req, res) => {
+        res.render('admin/competidores/novo.ejs', { id: req.params.id });
+    });
+
+    app.post('/admin/maratonas/:id/competidores/novo', (req, res) => {
+        var payload = req.body;
+        payload.Id = req.params.id;
+        create(payload)
+            .then(() => {
+                res.redirect('/admin/maratonas/' + req.params.id + '/competidores');
+            })
+            .catch(err => {
+                res.render('admin/competidores/novo.ejs', { errors: err });
+            });
+    });
+
+    app.get('/admin/maratonas/:id/competidores/:teamid/remove', (req, res) => {
+        remove(req.params.id, req.params.teamid).then(() => {
+            res.redirect('/admin/maratonas/' + req.params.id + '/competidores');
+        });
+    });
+
+    app.get('/admin/maratonas/:id/competidores/:teamid', (req, res) => {
+        get(req.params.id, req.params.teamid).then((result) => {
+            res.render('admin/competidores/edit.ejs', { team: result, id: req.params.id });
+        });
+    });
+
+    app.post('/admin/maratonas/:id/competidores/:teamid', (req, res) => {
+        update(req.params.id, req.params.teamid, req.body).then(() => {
+            res.redirect('/admin/maratonas/' + req.params.id + '/competidores');
         });
     });
 }
