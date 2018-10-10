07f4ce01901ca518908fe39f058212641315fe2e
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-10-10 19:31:47 -0300

    ajustes de testes para produção

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ competidoresRoutes(app);
 placarRoutes(app);
 juizRoutes(app);
 
-app.listen(80, function() {
+app.listen(process.env.port || 5000, function() {
     console.log('Server is running');
 });
