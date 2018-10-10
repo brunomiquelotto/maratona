3d89e79b84f2ef2d8c363c9dfbd6966406d0f537
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-10-10 19:32:23 -0300

    ajuste de porta

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ competidoresRoutes(app);
 placarRoutes(app);
 juizRoutes(app);
 
-app.listen(process.env.port || 5000, function() {
+app.listen(process.env.PORT || 5000, function() {
     console.log('Server is running');
 });
